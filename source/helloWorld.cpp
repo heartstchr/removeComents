92c4411f4972dc3c707ceaa159f4ac430bf98050
heartstchr <heartstchr@live.com>
2017-09-02 13:39:12 +0530

    [Comment Stripper]

---
+++ source/helloWorld.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+   /*
+   The return 0; statement is the "Exit status" of the program. In simple terms, program ends with this statement.
+   */
+}
